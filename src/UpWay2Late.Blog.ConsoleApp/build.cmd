d1aeb776a63064e452314514a2bbb7f4156b3579
James K <github-public@katic.org>
2016-10-27 16:54:02 -0700

    Add files to build for windows or osx.

---
+++ src/UpWay2Late.Blog.ConsoleApp/build.cmd
@@ -0,0 +1,7 @@
+﻿dotnet build -r osx.10.12-x64
+dotnet build -r win10-x64
+dotnet publish -c release -r osx.10.12-x64
+dotnet publish -c release -r win10-x64
+robocopy.exe D:\Dev\UpWay2Late\Blog\UpWay2Late.Blog.ConsoleApp\src\UpWay2Late.Blog.ConsoleApp D:\Dev\UpWay2Late\Blog\UpWay2Late.Blog.ConsoleApp\src\UpWay2Late.Blog.ConsoleApp\bin\release\netstandard1.6\osx.10.12-x64\publish appsettings.json /R:1 /W:5
+robocopy.exe D:\Dev\UpWay2Late\Blog\UpWay2Late.Blog.ConsoleApp\src\UpWay2Late.Blog.ConsoleApp D:\Dev\UpWay2Late\Blog\UpWay2Late.Blog.ConsoleApp\src\UpWay2Late.Blog.ConsoleApp\bin\release\netstandard1.6\win10-x64\publish appsettings.json /R:1 /W:5
+pause
