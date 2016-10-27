242fa1ed4de3affad3227c377bc6eb281de7ac80
James K <github-public@katic.org>
2016-10-26 21:23:29 -0700

    Adding the Hello World!

--- src/UpWay2Late.Blog.ConsoleApp/Program.cs
+++ src/UpWay2Late.Blog.ConsoleApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace UpWay2Late.Blog.ConsoleApp
 {
@@ -9,6 +6,9 @@ namespace UpWay2Late.Blog.ConsoleApp
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
