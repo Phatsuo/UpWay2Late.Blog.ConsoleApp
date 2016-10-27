70bcd07a99e370d07a0e6d05d4bc45a9f09a991f
James K <github-public@katic.org>
2016-10-26 21:14:48 -0700

    Add project files.

---
+++ src/UpWay2Late.Blog.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpWay2Late.Blog.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
